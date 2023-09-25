ce6377a0ebd0f98f215ceb66f3e3f364f51e6c0c
0xXiaoChen <1255552127@qq.com>
2023-09-25 14:50:07 +0800

    fix nextjs error

--- components/EmojiInput.jsx
+++ components/EmojiInput.jsx
@@ -1,9 +1,14 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import punycode from "punycode/";
-import Picker from "@emoji-mart/react";
 import data from "@emoji-mart/data";
+import Picker from "@emoji-mart/react";
+import { init } from "emoji-mart";
+
+init({ data });
 
 const EmojiInput = () => {
+  const [isMounted, setIsMounted] = useState(false);
+
   const [emoji, setEmoji] = useState([]);
   const [encoded, setEncoded] = useState([]);
   const [decoded, setDecoded] = useState([]);
@@ -17,6 +22,14 @@ const EmojiInput = () => {
   };
 
   const domain = `${decoded.join("")}.stark`;
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return null;
+  }
 
   return (
     <div>
